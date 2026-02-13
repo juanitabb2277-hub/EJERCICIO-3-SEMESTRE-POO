34e2e65099299225b2538c3c273e602575d3f912
juanita <juanitabb2277@gmail.com>
2026-02-13 17:17:18 -0500

    COMANDOS

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("COMANDOS DE GIT");
+        System.out.println("SOY JUANITA BAQUERO BELTRAN Y PROGRAMABA EN JAVA");
+        System.out.println("COMANDOS DE GIT LISTOS: ");
+        System.out.println("git add .");
+        System.out.println("git commit -m "" ");
+        System.out.println("git push ");
+        System.out.println("git switch");
 
 
     }
